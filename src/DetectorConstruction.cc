3e2504652af5d2dc76c02d2e4842eba4b77598fb
piti <piti118@gmail.com>
2011-09-30 04:13:03 -0700

    move crystal to static class member

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -1,37 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-// $Id: DetectorConstruction.cc,v 1.1 2010-10-18 15:56:17 maire Exp $
-// GEANT4 tag $Name: geant4-09-04-patch-02 $
-//
-// 
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #include "DetectorConstruction.hh"
 
 #include "G4Material.hh"
@@ -55,7 +21,9 @@
 #include <vector>
 #include <iostream>
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+const G4double DetectorConstruction::crystal_x = 3*cm;
+const G4double DetectorConstruction::crystal_y = 3*cm;
+const G4double DetectorConstruction::crystal_z = 13*cm;
 
 DetectorConstruction::DetectorConstruction():
   LYSO(0),Air(0),world_box(0),world_log(0),world_pv(0),
@@ -65,13 +33,9 @@ DetectorConstruction::DetectorConstruction():
   //and do nothing...
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 DetectorConstruction::~DetectorConstruction(){ 
     //do nothing
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
@@ -100,8 +64,6 @@ void DetectorConstruction::DefineMaterials()
   
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 G4VPhysicalVolume* DetectorConstruction::ConstructCalorimeter()
 {
   using std::string;
@@ -109,10 +71,6 @@ G4VPhysicalVolume* DetectorConstruction::ConstructCalorimeter()
   using std::cout;
   using std::endl;
   //all the dimensions
-  cout << "aaaaaa" << endl;
-  G4double crystal_x = 3*cm;
-  G4double crystal_y = 3*cm;
-  G4double crystal_z = 13*cm;
   
   G4double offset_x = 0*mm;
   G4double offset_y = 0*mm;
