c21dabdb50a29ca36575b1e33fe86b15d6977363
piti <piti118@gmail.com>
2011-09-30 04:08:37 -0700

    add randPos for giving out random position to the central crystal

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -38,7 +38,8 @@
 #include "G4VUserDetectorConstruction.hh"
 #include "globals.hh"
 #include <vector>
-
+#include <cstdlib>
+#include "G4ThreeVector.hh"
 class G4Box;
 class G4LogicalVolume;
 class G4VPhysicalVolume;
@@ -55,9 +56,16 @@ class DetectorConstruction : public G4VUserDetectorConstruction
     DetectorConstruction();
    ~DetectorConstruction();
 
-  public:
-     
     G4VPhysicalVolume* Construct();
+    
+    G4ThreeVector randPos(){
+      G4double x=((double)std::rand()/(double)RAND_MAX)*crystal_x-crystal_x/2;//shift back to center
+      G4double y=((double)std::rand()/(double)RAND_MAX)*crystal_y-crystal_y/2;
+      G4double z=0.;
+      G4ThreeVector toReturn(x,y,z);
+      return toReturn;
+    }
+    
     static inline unsigned int calorIndex(unsigned int irow, unsigned icol){return irow*numcol+icol;}
     static inline unsigned int calorMinId() {return idoffset+calorIndex(0,0);}
     static inline unsigned int calorMaxId(){return idoffset+calorIndex(numrow-1,numcol-1);}
@@ -68,6 +76,11 @@ class DetectorConstruction : public G4VUserDetectorConstruction
     static const unsigned int numrow = 9;//make them both odd so one crystal is at the center
     static const unsigned int numcol = 9;
     static const unsigned int idoffset = 1000;
+    
+    static const G4double crystal_x;
+    static const G4double crystal_y;
+    static const G4double crystal_z;
+    
     
     G4Material* LYSO;
     G4Material* Air;
